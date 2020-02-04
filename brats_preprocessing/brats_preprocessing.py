18cf09837e99b7c09c959fa781c5708f9d512e6f
John Colby <john.b.colby@gmail.com>
2020-02-04 11:02:18 -0800

    Use generic RadStudy class

--- brats_preprocessing/brats_preprocessing.py
+++ brats_preprocessing/brats_preprocessing.py
@@ -1,108 +1,23 @@
 import os
 import pkg_resources
 import shutil
-import tempfile
-import zipfile
-import nibabel as nib
 import requests
-import pydicom
-import glob
-from datetime import datetime
 import argparse
 import logging
 
-import rpy2.robjects as ro
-from rpy2.robjects import pandas2ri
-pandas2ri.activate()
-
 from nipype.interfaces import fsl
-
-import pandas as pd
 
 from .pipelines import dcm2nii, non_t1, merge_orient
 
-class tumor_study():
+from rad_apps.radstudy import RadStudy
+
+class TumorStudy(RadStudy):
     def __init__(self, acc='', zip_path='', model_path='', n_procs=4):
-        self.zip_path     = zip_path
-        self.model_path   = model_path
-        self.dir_tmp      = ''
-        self.dir_study    = ''
-        self.channels     = ['flair', 't1', 't1ce', 't2']
-        self.series_picks = pd.DataFrame({'class': self.channels,
-                                          'prob': '',
-                                          'SeriesNumber': '',
-                                          'series': ''})
-        self.MNI_ref      = fsl.Info.standard_image('MNI152_T1_1mm_brain.nii.gz')
-        self.brats_ref    = pkg_resources.resource_filename(__name__, 'brats_ref_reorient.nii.gz')
-        self.n_procs      = n_procs
-        self.acc          = acc
-        self.hdr          = ''
-        self.study_date   = ''
-        assert self.acc or self.zip_path, 'No input study provided.'
-
-    def download(self, URL, cred_path):
-        """Download study via AIR API"""
-        import air_download.air_download as air
-        import argparse
-
-        assert not self.zip_path, '.zip path already available.'
-        assert self.dir_tmp, 'Working area not setup yet.'
-        args = argparse.Namespace()
-        args.URL = URL
-        args.acc = self.acc
-        args.cred_path = cred_path
-        args.profile = -1
-        args.output = os.path.join(self.dir_tmp, f'{self.acc}.zip')
-        air.main(args)
-        self.zip_path = args.output
-        self._extract()
-
-    def _extract(self):
-        """Extract study archive"""
-        assert not self.dir_study, 'dir_study already exists.'
-        dir_study = os.path.join(self.dir_tmp, 'dcm')
-        os.mkdir(dir_study)
-        zip_ref = zipfile.ZipFile(self.zip_path, 'r')
-        zip_ref.extractall(path = dir_study)
-        self.dir_study = os.path.join(dir_study, os.listdir(dir_study)[0])
-
-    def setup(self):
-        """Setup study for processing"""
-        # Create temporary working directory
-        if not self.dir_tmp:
-            self.dir_tmp = tempfile.mkdtemp()
-            os.mkdir(os.path.join(self.dir_tmp, 'nii'))
-
-        # Extract study archive
-        if not self.dir_study and self.zip_path:
-            self._extract()
-
-        # Load representative DICOM header
-        if self.dir_study and not self.hdr:
-            dcm_path = glob.glob(f'{self.dir_study}/*/*.dcm', recursive=True)[0]
-            self.hdr = pydicom.read_file(dcm_path)
-            self.acc = self.hdr.AccessionNumber
-            self.study_date = datetime.strptime(self.hdr.StudyDate, '%Y%m%d').strftime('%m/%d/%Y')
-
-    def classify_series(self):
-        """Classify series into modalities"""
-        ro.r['library']('dcmclass')
-        ro.r['load'](self.model_path)
-
-        self.series_picks = ro.r['predict_headers'](os.path.dirname(self.dir_study), ro.r['models'], ro.r['tb_preproc'])
-        paths = [os.path.abspath(os.path.join(self.dir_study, series)) for series in self.series_picks.series.tolist()]
-        self.series_picks['series'] = paths
-
-    def add_paths(self, paths):
-        """Manually specify directory paths to required series"""
-        self.series_picks.series = paths
-
-    def series_to_path(self, series):
-        """Convert a SeriesNumber to path"""
-        ro.r['library']('dcmclass')
-        ro.r['library']('dplyr')
-        tb = ro.r['load_study_headers'](os.path.join(self.dir_tmp, 'dcm'), 'SeriesNumber')
-        return tb.loc[tb['SeriesNumber'] == series]['path'][0]
+        super().__init__(acc, zip_path, model_path)
+        self.app_name  = 'gbm'
+        self.MNI_ref   = fsl.Info.standard_image('MNI152_T1_1mm_brain.nii.gz')
+        self.brats_ref = pkg_resources.resource_filename(__name__, 'brats_ref_reorient.nii.gz')
+        self.n_procs   = n_procs
 
     def preprocess(self, mni_mask = False, do_bias_correct = False):
         """Preprocess clinical data according to BraTS specs"""
@@ -146,39 +61,9 @@ class tumor_study():
                           out_file = os.path.join(self.dir_tmp, 'output', 'mask_mni.nii.gz'),
                           out_matrix_file = os.path.join(self.dir_tmp, 'output', 'mask_mni.mat'))
         FLIRT.run()
-
-    def report(self):
-        """Generate PDF report"""
-        ro.r['library']('ucsfreports')
-        params = ro.ListVector({'input_path':   self.dir_tmp,
-                                'patient_name': self.hdr.PatientName.family_comma_given(),
-                                'patient_MRN':  self.hdr.PatientID,
-                                'patient_acc':  self.hdr.AccessionNumber,
-                                'study_date':   self.study_date})
-        ro.r['ucsf_report']('gbm', output_dir = os.path.join(self.dir_tmp, 'output'), params = params)
-
-    def copy_results(self, output_dir = '.'):
-        src = os.path.join(self.dir_tmp, 'output')
-        dest = os.path.join(output_dir, self.acc)
-        assert not os.path.exists(dest), 'Output directory already exists.'
-        shutil.copytree(src, dest)
+        # Save a template itksnap workspace
         itk_file = pkg_resources.resource_filename(__name__, 'workspace.itksnap')
-        shutil.copy(itk_file, dest)
-
-    def __str__(self):
-        s_picks = str(self.series_picks.iloc[:, 0:3]) if not self.series_picks.empty else ''
-        s = ('Brain Tumor object\n'
-            f'  Accession #: {self.acc}\n'
-            f'  dir_tmp: {self.dir_tmp}\n'
-            f'  Series picks:\n{s_picks}')
-        return s
-
-    def rm_tmp(self):
-        """Remove temporary working area"""
-        if not self.dir_tmp == '':
-            shutil.rmtree(self.dir_tmp)
-        else:
-            print('Nothing to remove.')
+        shutil.copy(itk_file, os.path.join(self.dir_tmp, 'output'))
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -195,7 +80,7 @@ def parse_args():
 
 def process_gbm(args):
     try:
-        mri = tumor_study(acc = args.acc, model_path = args.model_path)
+        mri = TumorStudy(acc = args.acc, model_path = args.model_path)
         mri.setup()
         mri.download(URL = args.air_url, cred_path = args.cred_path)
         mri.setup()
--- setup.py
+++ setup.py
@@ -8,7 +8,11 @@ setup(
     author_email='john.b.colby@gmail.com',
     description='Tools for preprocessing clinical data according to BraTS specification',
     packages=['brats_preprocessing'],
-    install_requires=['nibabel', 'nipype', 'pandas', 'requests', 'pydicom'],
+    install_requires=[
+        'rad_apps @ git+https://git.radiology.ucsf.edu/jcolby/rad_apps.git#egg=rad_apps',
+        'nipype',
+        'requests',
+    ],
     extras_require={'classify_series': ['rpy2', 'tzlocal'], 'report': ['rpy2', 'tzlocal'], 'download': ['air_download']},
     include_package_data=True,
     entry_points={'console_scripts': ['process_gbm = brats_preprocessing.brats_preprocessing:cli']},
